3dfd95a87ee8a0c62293c406e38f7350a0df41d4
Nubberino <ignatius.sarwono@student.umn.ac.id>
2024-03-13 10:25:56 +0700

    Initial Data

---
+++ footer.js
@@ -0,0 +1,69 @@
+class Footer extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback() {
+    this.innerHTML = `<footer class="site-footer footer-large footer-dark text-white footer-style3" style="display: block; height: 416px;">
+					<!-- FOOTER BLOCKES START -->  
+					<div class="footer-top">
+						<div class="container">
+							<div class="row">
+																	<div id="about-2" class="col-lg-3 col-md-6 col-sm-12 widget_about industroz-widget"><div class="widget footer-widget">	
+			
+			<div class="logo-footer clearfix">
+				<a href="https://ptnes.net/">
+					<img src="index_files/PT-Nes-logo-putih.png" alt="img">
+				</a>
+			</div>
+			<p>Industroz industry has very good strength in innovative technology and tools San Francisco City Hall, San Francisco, CA.</p>
+							<ul class="social">
+											<li><a class="social-icon-footer-custom grid-small grid-linkedin" href="https://www.linkedin.com/company/pt-national-energy-solutions/mycompany/"><i class="fa fa-linkedin-square"></i></a></li>
+												<li><a class="social-icon-footer-custom grid-small grid-instagram" href="https://www.instagram.com/nesgroup_official?igsh=NTE2YXA3aXRvbm84"><i class="fa fa-instagram"></i></a></li>
+										</ul>
+						</div></div><div id="nav_menu-4" class="col-lg-3 col-md-6 col-sm-12 widget_nav_menu industroz-widget"><div class="widget footer-widget"><h3 class="widget-title">Projects</h3><div class="menu-projects-new-home-container"><ul id="menu-projects-new-home" class="menu"><li id="menu-item-726" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-726"><a href="https://ptnes.net/projects/industry-complex/">Industry Complex</a></li>
+<li id="menu-item-727" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-727"><a href="https://ptnes.net/projects/oil-pipeline-project/">Oil Pipeline Project</a></li>
+<li id="menu-item-728" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-728"><a href="https://ptnes.net/projects/gear-manufacturing/">Gear Manufacturing</a></li>
+<li id="menu-item-729" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-729"><a href="https://ptnes.net/projects/machine-engineering/">Machine Engineering</a></li>
+<li id="menu-item-730" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-730"><a href="https://ptnes.net/projects/warehouse-industry/">Warehouse Industry</a></li>
+<li id="menu-item-731" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-731"><a href="https://ptnes.net/projects/gothia-mining-factory/">Gothia Mining Factory</a></li>
+</ul></div></div></div><div id="nav_menu-6" class="col-lg-3 col-md-6 col-sm-12 widget_nav_menu industroz-widget"><div class="widget footer-widget"><h3 class="widget-title">Services</h3><div class="menu-services-new-home-container"><ul id="menu-services-new-home" class="menu"><li id="menu-item-735" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-735"><a href="https://ptnes.net/services/we-fight-pollution/">We Fight Pollution</a></li>
+<li id="menu-item-736" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-736"><a href="https://ptnes.net/services/oil-pump/">Oil Pump</a></li>
+<li id="menu-item-737" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-737"><a href="https://ptnes.net/services/thermal-power/">Thermal Power</a></li>
+<li id="menu-item-738" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-738"><a href="https://ptnes.net/services/oil-resources/">Oil Resources</a></li>
+<li id="menu-item-741" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-741"><a href="https://ptnes.net/services/oil-factory/">Oil Factory</a></li>
+<li id="menu-item-999" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-999"><a href="https://ptnes.net/services/chemical-analysis/">Chemical Analysis</a></li>
+</ul></div></div></div><div id="contact_info-2" class="col-lg-3 col-md-6 col-sm-12 widget_contact_info industroz-widget"><div class="widget footer-widget"><h3 class="widget-title">Contact Us</h3>		
+		<div class="contact-footer-widget">
+							<div class="industroz-contact"><i class="fa fa-map-marker"></i>Jl. Letjen S. Parman No.Kav 77, RT.6/RW.3, 3, Slipi, Jakarta Barat</div>
+							
+							<div class="industroz-contact"><i class="fa fa-envelope-o"></i>
+											<a href="mailto:info@ptnes.net"> info@ptnes.net</a>
+															</div>
+							<div class="industroz-contact"><i class="fa fa-phone"></i>
+									<a href="tel:(021) 5363070">(021) 5363070</a>
+												</div>
+		</div>
+		</div></div>                              
+																</div> 
+						</div>
+					</div>
+					<!-- FOOTER COPYRIGHT -->
+					
+					<div class="footer-bottom">
+						<div class="container">
+							<div class="wt-footer-bot-left d-flex justify-content-between">
+								<span class="copyrights-text">Copyright © 2024 PT National Energy Solutions</span>
+								<ul id="menu-footer-links" class="copyrights-nav"><li id="menu-item-229" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-229"><a rel="privacy-policy" href="https://ptnes.net/privacy-policy/">Privacy Policy</a></li>
+<li id="menu-item-230" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-230"><a href="https://ptnes.net/contact-us/">Contact Us</a></li>
+</ul>   
+							</div>
+						</div>   
+					</div>  
+				</footer>
+				<!-- FOOTER END -->
+						</div>
+			`;
+}
+}
+customElements.define('footer-component', Footer);
---
+++ header.js
@@ -0,0 +1,93 @@
+class Header extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback() {
+    this.innerHTML = `<header class="site-header header-style-2 inner-pages-header mobile-sider-drawer-menu">
+									<div class="top-bar site-bg-secondry">
+						<div class="container">
+
+							<div class="d-flex justify-content-between">
+								<div class="wt-topbar-left d-flex flex-wrap align-content-start">
+									
+									<ul class="wt-topbar-info e-p-bx text-white">
+										<li>Monday - Friday 8AM -5PM</li>
+									</ul>
+								</div>
+								
+								<div class="wt-topbar-right d-flex flex-wrap align-content-center">
+									<ul class="wt-topbar-info-2 e-p-bx text-white">
+										<li><i class="fa fa-phone"></i>(+6221) 5363070</li>
+										<li><i class="fa fa-envelope"></i>info@nesgroup.id</li>
+									</ul> 
+					
+									<ul class="social-icons">
+																					<li><a href="https://www.linkedin.com/company/pt-national-energy-solutions/mycompany/" class="fa fa-linkedin"></a></li>
+																						<li><a href="https://www.instagram.com/nesgroup_official?igsh=NTE2YXA3aXRvbm84" class="fa fa-instagram"></a></li>
+																				</ul> 
+								</div>
+							</div>
+						</div>
+					</div> 
+									<div class="sticky-wrapper" style="height: 100px;"><div class="sticky-header main-bar-wraper navbar-expand-lg is-fixed color-fill">
+					<div class="main-bar">                       
+						<div class="container clearfix"> 
+							<div class="logo-header">
+								<div class="logo-header-inner logo-header-one">
+									<a href="https://ptnes.net/">
+										<img src="index_files/PT-Nes-logo-warna.png" alt="PT National Energy Solutions">
+									</a>
+								</div>
+							</div>  
+							
+							<!-- NAV Toggle Button -->
+							<button id="mobile-side-drawer" data-target=".header-nav" data-toggle="collapse" type="button" class="navbar-toggler collapsed">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar icon-bar-first"></span>
+								<span class="icon-bar icon-bar-two"></span>
+								<span class="icon-bar icon-bar-three"></span>
+							</button> 
+							<div class="extra-nav header-2-nav">
+								<div class="extra-cell">
+																	</div>
+							</div>                                                                       
+
+							<!-- MAIN Vav -->
+							<div class="nav-animation header-nav navbar-collapse collapse d-flex justify-content-center">
+								<ul id="menu-main-menu" class="nav navbar-nav"><li id="menu-item-922" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-922"><a href="https://ptnes.net/">Home</a></li>
+<li id="menu-item-34" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-34 has-child"><a href="#">Our Story</a><div class="fa fa-angle-right submenu-toogle"></div>
+<ul class="sub-menu">
+	<li id="menu-item-332" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-332"><a href="https://ptnes.net/about-us-1/">Milestone</a></li>
+	<li id="menu-item-343" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-343"><a href="https://ptnes.net/about-us-2/">Vision &amp; Mission</a></li>
+	<li id="menu-item-1084" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1084"><a href="http://ptnes.net/">BOD &amp; BOC</a></li>
+</ul>
+</li>
+<li id="menu-item-35" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-35 has-child"><a href="#">Services</a><div class="fa fa-angle-right submenu-toogle"></div>
+<ul class="sub-menu">
+	<li id="menu-item-344" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-344"><a href="https://ptnes.net/services-one/">Coating Manufacture</a></li>
+	<li id="menu-item-342" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-342"><a href="https://ptnes.net/services-two/">Energy &amp; Power</a></li>
+	<li id="menu-item-376" class="menu-item menu-item-type-post_type menu-item-object-services menu-item-376"><a href="https://ptnes.net/services/we-think-about-consumer/">Services Detail</a></li>
+</ul>
+</li>
+<li id="menu-item-36" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-36 has-child"><a href="#">Project</a><div class="fa fa-angle-right submenu-toogle"></div>
+<ul class="sub-menu">
+	<li id="menu-item-353" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-353"><a href="https://ptnes.net/project-grid/">Project Grid</a></li>
+	<li id="menu-item-351" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-351"><a href="https://ptnes.net/project-carousel/">Project Carousel</a></li>
+	<li id="menu-item-374" class="menu-item menu-item-type-post_type menu-item-object-projects menu-item-374"><a href="https://ptnes.net/projects/mechanical-engineering/">Project Detail</a></li>
+</ul>
+</li>
+<li id="menu-item-356" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-356"><a href="https://ptnes.net/our-team/">Affiliates</a></li>
+<li id="menu-item-369" class="menu-item menu-item-type-post_type menu-item-object-page page-item-361 "><a href="https://ptnes.net/blog-grid/" aria-current="page">News &amp; Article</a></li>
+<li id="menu-item-1104" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1104"><a href="#">Certificates</a></li>
+<li id="menu-item-158" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-158"><a href="https://ptnes.net/contact-us/">Contact Us</a></li>
+</ul>
+							</div>
+													</div>    
+					</div>  
+				</div></div>
+				
+			</header>`;
+}
+}
+customElements.define('header-component', Header);
